769787697c80d41a5fc91698f12cce8252787767
AlcaUDESA <agustinlucass03@gmail.com>
2022-11-02 23:52:02 -0300

    Register con Boton formulario vacio. Falta funcionalidad de errors. El resto anda firebase

--- src/screens/Registro.js
+++ src/screens/Registro.js
@@ -1,17 +1,62 @@
 import React, {Component} from 'react';
+import {auth, db} from '../firebase/config';
 import {View, Text, StyleSheet, TextInput, TouchableOpacity, Image} from 'react-native';
 
 class Registro extends Component {
     constructor(){
         super()
+
+        //Seteamos un state inicial cero para lograr los inputs vacios. Tambien creamos propiedad errors para el catch//
         this.state = {
-            email: '',
-            usuario: '',
-            contraseña: '',
-            bio: '',
-            foto: ''
+            email: "",
+            contraseña: "",
+            usuario: "",
+            bio: "",
+            foto: "",
+            errors: ""
         }
     }
+
+
+registrarUsuario(email,pass, userName, bio, foto){
+    //Lo que queremos hacer es registrar en Firebase que damos de alta al usuario y si el registro sale bien entonces redireccionar a Login //
+
+    //Hacemos uso de auth de firebase y aprovechamos el método createUserWithEmailAndPassword con los parámetros obligatorios que son email y pass//
+    auth.createUserWithEmailAndPassword(email,pass)
+        .then(res =>{
+            //Este método es asincronico por lo que retorna una promise, si se cumple la promise entonces tenemos sucede un then. En el then hacemos uso de add para agregar la coleccion users y dar de alta al usuario con los valores propios de este usuari//
+                db.collection("users").add({
+                    owner:email,
+                    userName: userName, 
+                    bio: bio,
+                    foto: foto,
+                    createdAt: Date.now()
+                })
+                //Add tambien retorna una promesa por lo que si se cumple devolverá lo que suceda en el then
+                .then(()=>{
+                    this.setState({
+                        email: "",
+                        contraseña: "",
+                        usuario: "",
+                        bio: "",
+                        foto: "",
+                        errors: ""
+                    })
+                    //Redireccionamiento a login//
+                    this.props.navigation.navigate("Portada")
+                })
+                .catch(error => console.log(error))    
+        })
+        .catch(error => this.setState({
+            errors: Response.errors
+        })
+        )}
+
+    
+
+
+
+
 
     render(){
         return(
@@ -23,16 +68,9 @@ class Registro extends Component {
                     resizeMode = 'contain'
                 />
 
-                <Text style={styles.titulo}>Registrate</Text>
+                <Text style={styles.titulo}>Regístrate</Text>
 
                 <View style={styles.form}>
-                    <TextInput 
-                        placeholder= 'Nombre de Usuario'
-                        keyboardType= 'default'
-                        onChangeText={ texto => this.setState({usuario : texto})}
-                        value = {this.state.usuario}
-                        style={styles.campo}
-                    />
                     <TextInput 
                         placeholder= 'Email'
                         keyboardType= 'email-address'
@@ -49,6 +87,13 @@ class Registro extends Component {
                         style={styles.campo}
                     />
                     <TextInput 
+                        placeholder= 'Nombre de Usuario'
+                        keyboardType= 'default'
+                        onChangeText={ texto => this.setState({usuario : texto})}
+                        value = {this.state.usuario}
+                        style={styles.campo}
+                    />
+                    <TextInput 
                         placeholder= 'Biografía'
                         keyboardType= 'default'
                         onChangeText={ texto => this.setState({bio : texto})}
@@ -61,15 +106,26 @@ class Registro extends Component {
                         onChangeText={ texto => this.setState({foto : texto})}
                         value = {this.state.foto}
                         style={styles.campo}
-                    />                  
+                    />    
 
-                    <TouchableOpacity onPress={ () => this.registrarUsuario ( this.state.usuario, this.state.email, this.state.contraseña, this.state.bio, this.state.foto)}>
+
+
+
+            {
+                this.state.email =="" || this.state.contraseña =="" ? 
+                    <TouchableOpacity>
+                        <Text style={styles.botonerror}>Registrarme</Text>
+                    </TouchableOpacity>
+                :
+                    <TouchableOpacity onPress={ () => this.registrarUsuario ( this.state.email, this.state.contraseña, this.state.usuario, this.state.bio, this.state.foto)}>
                         <Text style={styles.boton}>Registrarme</Text>
                     </TouchableOpacity>
-
-                    <Text onPress={ () => this.props.navigation.navigate ('Inicio')} style={styles.link}>¿Ya tenes una cuenta? Inicia Sesión</Text>
+            }
+                    <Text onPress={ () => this.props.navigation.navigate ("Portada")} style={styles.link}>¿Ya tienes una cuenta? Inicia Sesión</Text>
                 </View>
             </View>
+        
+
         )
     }
 }
@@ -105,7 +161,16 @@ const styles = StyleSheet.create({
         fontFamily: 'Courier',
         fontSize: 14,
         margin: 10,
-        backgroundColor: 'rgb(234,252,255)',
+        backgroundColor: 'rgb(0,21,247)',
+        borderRadius: 10,
+        textAlign: 'center',
+        padding: 5
+    },
+    botonerror: {
+        fontFamily: 'Courier',
+        fontSize: 14,
+        margin: 10,
+        backgroundColor: 'rgb(105,105,105)',
         borderRadius: 10,
         textAlign: 'center',
         padding: 5
