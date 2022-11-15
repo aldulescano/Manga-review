218c02e9de0f06b125548bc62b7da5b4ef5cf87f
AlcaUDESA <agustinlucass03@gmail.com>
2022-11-15 05:51:10 -0300

    Icono de loading appjson y comentarios terminado

--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -2,48 +2,70 @@ import React, { Component } from 'react'
 import firebase from 'firebase'
 import {db, auth} from "../firebase/config"
 import { Text, View, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native'
-import Posteo from "../components/Posteo"
-import TouchHistoryMath from 'react-native/Libraries/Interaction/TouchHistoryMath'
-import Subircomentario from '../components/Subircomentario'
 
 
 class Comments extends Component {
 constructor (props){
     super (props)
     this.state = {
-    id:this.props.route.params.id,
-    data:"",
-    comentario:"",
-    comentarios:this.props.comentarios
+        id:this.props.route.params.id,
+        comentario:"",
+        comentarios: [],
     }
 }
 
 componentDidMount(){
-    db.collection('posteos').doc(this.state.id).onSnapshot(doc=> {this.setState({
-        data: doc.data(),  
-    }
-    )
-    })
+    db.collection('posts').doc(this.state.id).onSnapshot(
+        docs => {
+            this.setState({
+                comentarios: docs.data().comentarios
+            })
+        })
+};
+
+subirComentario(comentario){
+    db.collection("posts")
+    .doc(this.state.id)
+    .update({
+        comentarios: firebase.firestore.FieldValue.arrayUnion({owner:auth.currentUser.email,comentario:comentario,createdAt: Date.now()})  
+})
+.then(() => {
+    this.setState({
+        comentario: "",     
+}) })
 }
 
 
     render(){
         return(
             <View> 
-            <Text> Comentarios del posteo actual </Text>
+            <Text> Comentarios del post actual</Text>
 
             {this.state.comentarios == 0 ?
             
             <View style={styles.container}> 
             <Text  > Aún no hay comentarios. Sé el primero en opinar </Text>
-            <Subircomentario id={this.state.id} data={this.state.data}/>
+            <SubirComentario id={this.state.id}/>
             </View>
             :
             <FlatList 
-            data={this.state.comentarios}
-            keyExtractor={ unComentario => unComentario.createdAt.toString()}
-            renderItem={ ({item}) => <Text>{item.creador} comentó: {item.comentarios}</Text>}
+                data={this.state.comentarios}
+                keyExtractor={ unComentario => unComentario.createdAt.toString()}
+                renderItem={ ({item}) => <Text>{item.creador} comentó: {item.comentarios}</Text>}
             /> 
+            }
+            <TextInput 
+                placeholder='Agregue un comentario'
+                keyboardType='default'
+                onChangeText={comentario=> this.setState({comentario:comentario})}
+                value={this.state.comentario}
+            />
+            {this.state.comentario === "" ?
+                <></>
+                :
+                <TouchableOpacity onPress={()=> this.subirComentario(this.state.comentario) }>
+                    <Text>Subir comentario a este posteo</Text>
+                </TouchableOpacity> 
 
         
 
